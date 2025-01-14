e5b93165cb3efa87fbc0ce72938e94d1f802f813
om <ombhandwalkar38126@gmail.com>
2025-01-14 17:05:38 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+def sum(a, b):
+    return a+b
+
+
+def sub(a, b):
+    return a-b
+
+
+def mul(a, b):
+    return a*b
+
+
+result1 = sum(2, 4)
+result2 = sub(4, 2)
+result3 = mul(2, 4)
+
+print(result1, result2, result3)
